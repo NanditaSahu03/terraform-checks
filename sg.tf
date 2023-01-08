acf5fcd194e918d8299680796aa83ad2c37bc0be
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2023-01-08 18:37:02 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "allow_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = var.cidr_blocks
+    cidr_blocks      = var.cidr_blocks #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
 
   egress {
@@ -15,7 +15,7 @@ resource "aws_security_group" "allow_sg" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]  #tfsec:ignore:aws-ec2-no-public-egress-sgr
     ipv6_cidr_blocks = ["::/0"]
   }
 
