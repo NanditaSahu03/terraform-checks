e921aca5b009fa761f5d73619a29467f6c81b755
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2023-01-08 18:37:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,4 +5,9 @@ resource "aws_instance" "web" {
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_sg.id]
   iam_instance_profile   = var.iam_instance_profile
+  
+  #tfsec:ignore:aws-ec2-enforce-http-token-imds 
+  metadata_options {
+    
+  }
 }
